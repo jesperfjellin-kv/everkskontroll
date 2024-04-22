7e53ccda67aa98f0a251ffbd3e61a1e8e18497d2
Jesper Fjellin <145996132+jesperfjellin-kv@users.noreply.github.com>
2024-04-22 13:51:26 +0200

    Add files via upload

---
+++ WinSCP/sync_winscp.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\WinSCP\WinSCP.com" /script="C:\FKKOslo\PyScripts\Download_from_Nextcloud\sync_winscp.txt" /log="C:\FKKOslo\PyScripts\Download_from_Nextcloud\WinSCP.log" /ini=nul
