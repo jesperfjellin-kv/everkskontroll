7e5ad2507fa1505de0cdbf1e7c7b64f5f5a11e49
Jesper Fjellin <145996132+jesperfjellin-kv@users.noreply.github.com>
2024-04-22 13:52:40 +0200

    Delete sync_winscp.bat

--- sync_winscp.bat
+++
@@ -1,1 +0,0 @@
-"C:\Program Files (x86)\WinSCP\WinSCP.com" /script="C:\FKKOslo\PyScripts\Download_from_Nextcloud\sync_winscp.txt" /log="C:\FKKOslo\PyScripts\Download_from_Nextcloud\WinSCP.log" /ini=nul
